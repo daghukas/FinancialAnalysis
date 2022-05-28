86e5d8b3d3232645ff3cdf2f64805c1cb22da8b8
daghukas <david.ghukasyan@student.hs-rm.de>
2022-05-28 10:27:33 +0200

    Start of merging the two existing for loops

--- calcformations.py
+++ calcformations.py
@@ -55,24 +55,14 @@ def detectDoubleTop(df):
 	datasetIndex = df.index.tolist()
 	datasetCloseVal = df[['Close']]
 
-	# Polyregression der Close-Daten, curve-fitting
-	#polyfit = np.polyfit(datasetIndex, datasetCloseVal, 5)
-	#yvalpoly = np.polyval(polyfit, datasetIndex)
-	#print(yvalpoly)
 	fig1 = plt.figure(figsize=(80, 40), dpi= 120, facecolor='w', edgecolor='k')
 	
 	# get indizes of maxs and mins
 	arrIndexMaxima = sp.argrelextrema(np.array(datasetCloseVal), np.greater)
 	arrIndexMinima = sp.argrelextrema(np.array(datasetCloseVal), np.less)
-	#print("Maximum ")
-	#print(arrIndexMaxima[0])
-	#print(arrIndexMinima[0])
 
 	arrValuesMaxima = datasetCloseVal.iloc[arrIndexMaxima[0]]['Close'].values
 	arrValuesMinima = datasetCloseVal.iloc[arrIndexMinima[0]]['Close'].values
-
-	#print(arrValuesMaxima)
-	#print(arrValuesMinima)
 
 	# get values of minimums/maximums
 	max_vals = []
@@ -80,9 +70,7 @@ def detectDoubleTop(df):
 	max_values = []
 	min_vals = []
 	min_vals_index = []
-	plt.xticks(range(0, df.size))
-	#print(np.where(df.index.values > 5, df.index.values))
-	
+	plt.xticks(range(0, df.size))	
 	
 	# index = Index innerhalb der Index-Schleife;i = Index wo Maximum vorliegt
 	for index, i in enumerate(arrIndexMaxima[0]):		
@@ -102,11 +90,9 @@ def detectDoubleTop(df):
 								
 				rangeArr = df.index.values[rangeBetweenTwoMaxs]
 				valuesOfRange = datasetCloseVal[rangeBetweenTwoMaxs]
-				#print(rangeArr) # get bis i+5 points
-				#print(valuesOfRange)
 				necklineValue = np.min(valuesOfRange[:-4])[0] # min
 				startNeckline = min(rangeArr)*(1/datasetCloseVal.size)
-				endNeckline = (max(rangeArr))*(1/datasetCloseVal.size)
+				endNeckline = max(rangeArr)*(1/datasetCloseVal.size)
 				plt.axhline(y=necklineValue, xmin=startNeckline, xmax=endNeckline, color='red', alpha=0.1)
 				
 				# wenn unter Neckline faellt, dann ists DoubleTop
@@ -115,45 +101,77 @@ def detectDoubleTop(df):
 				if firstIndexBelowNeckline > -1:	
 					indexBreakthrough = arrIndexMaxima[0][index] + firstIndexBelowNeckline
 					valueBreakthrough = datasetCloseVal[df.index.values == indexBreakthrough]['Close'].values
-					#print(valueBreakthrough)
 					plt.plot(indexBreakthrough, valueBreakthrough, 'o', alpha=1, color='blue')
 					plt.plot(valuesOfRange.index.values, valuesOfRange.values, '-', color='green', markersize=4, alpha=1)
 					print("Detected Double Top:")
 					print(valuesOfRange.index.values)
 	print("")
 	print("DOUBLE BOTTOM")
-	# doubleBottom
+	# doubleBottom iterate through all minima
 	for index, i in enumerate(arrIndexMinima[0]):
 		if index > 0:
-			if (arrValuesMinima[index-1]>=arrValuesMinima[index]) and math.isclose(arrValuesMinima[index-1],arrValuesMinima[index],rel_tol=0.02):
-				print("I-1 ", arrIndexMinima[0][index-1], "I ", arrIndexMinima[0][index])
-				min_vals.append(arrValuesMinima[index-1])
-				min_vals_index.append(arrIndexMinima[0][index-1])
-				min_vals.append(arrValuesMinima[index])
-				min_vals_index.append(i)
+			# if prev minima bigger eq curr minima & rel_diff 2 percent
+			if (arrValuesMinima[index-1]<=arrValuesMinima[index]) and math.isclose(arrValuesMinima[index-1],arrValuesMinima[index],rel_tol=0.02):
+				
+				min_vals.append(arrValuesMinima[index-1]) # prev minimum
+				min_vals_index.append(arrIndexMinima[0][index-1]) # index of pre minimum
+				min_vals.append(arrValuesMinima[index]) # curr minimum
+				min_vals_index.append(i) # index of curr minimum
+				
+				# rangetoexamine
 				rangeBetweenTwoMins = (df.index.values >= arrIndexMinima[0][index-1]) & (df.index.values < arrIndexMinima[0][index]+5)
 				arrRangeMins = df.index.values[rangeBetweenTwoMins]
 				arrValuesOfMins = datasetCloseVal[rangeBetweenTwoMins]
-				print(arrValuesOfMins)
-				necklineValue = np.max(arrValuesOfMins[:-4])[0] #max
+				necklineValue = np.max(arrValuesOfMins[:-4])[0] 
 				startNeckline = min(arrRangeMins)*(1/datasetCloseVal.size)
 				endNeckline = max(rangeArr)*(1/datasetCloseVal.size)
 
 				firstIndexBreakingNeckline = getFirstIndexBreakingNeckline(arrValuesOfMins[-4:]['Close'].values, necklineValue, ">")
 				if firstIndexBreakingNeckline > -1:
+					#print(arrValuesOfMins)
 					indexBreakthrough = arrIndexMinima[0][index] + firstIndexBreakingNeckline
 					valueBreakthrough = datasetCloseVal[df.index.values == indexBreakthrough]['Close'].values
-					print("Detected Double Bottom: ")
-					print(arrValuesOfMins.index.values)
+					#print("Detected Double Bottom: ")
+					#print(arrValuesOfMins.index.values)
 
 	# Plot data Points
 	plt.plot(datasetIndex, datasetCloseVal, '-', markersize=1.5, color='black', alpha=0.6)
-	#plt.plot(datasetIndex, yvalpoly, label='fit')
 	plt.plot(max_vals_index, max_vals, 'o', markersize=9.5, color='green')
-	#plt.plot(arrIndexMinima[0], min_vals, 'o', markersize=9.5, color='red')
-	#plt.xticks(np.arange(len(datasetCloseVal)), np.arange(1, len(datasetCloseVal)+1))
 	plt.xlim([0, datasetCloseVal.size])
 	fig1.savefig("test2.png")
+
+	detectDoubleFormation(arrIndexMaxima[0], arrValuesMaxima, 0, df)
+
+def detectDoubleFormation(arrIndexExtremeValues, arrValsExtremeValues, typeOfDoubleFormation, df):
+	# 0 = Double Top; 1 = Double Bottom; 2 = Both
+	
+	datasetCloseVal = df[['Close']]
+
+	critCompareExtremeVals = '>=' #(prevExtreme >= currExtreme)
+	necklineOperator = "<"
+	if typeOfDoubleFormation == 1:
+		critCompareExtremeVals = '<=' #(prevExtreme <= currExtreme)
+		necklineOperator = ">"
+
+	for indexArr, indexDataset in enumerate(arrIndexExtremeValues):
+		if indexArr > 0:
+			prevExtreme = arrValsExtremeValues[indexArr-1]
+			currExtreme = arrValsExtremeValues[indexArr]
+			
+			if (eval(str(prevExtreme) + critCompareExtremeVals + str(currExtreme)) and math.isclose(prevExtreme, currExtreme, rel_tol=0.02)):
+				
+				conditionRangeBetweenTwoExtremes = (df.index.values >= arrIndexExtremeValues[indexArr-1]) & (df.index.values < arrIndexExtremeValues[indexArr]+5)
+				
+				arrRangeExtremes = df.index.values[conditionRangeBetweenTwoExtremes]
+				arrValuesOfExtremes = datasetCloseVal[conditionRangeBetweenTwoExtremes]
+				necklineValue = np.max(arrValuesOfExtremes[:-4])[0]
+				firstIndexBreakingNeckline = getFirstIndexBreakingNeckline(arrValuesOfExtremes[-4:]['Close'].values, necklineValue, necklineOperator)
+				if firstIndexBreakingNeckline > -1: 
+					indexBreakthrough = currExtreme + firstIndexBreakingNeckline
+					valueBreakthrough = datasetCloseVal[df.index.values == indexBreakthrough]['Close']
+					print("Detected Double Formations: ")
+					print(arrValuesOfExtremes.index.values)
+
 
 def getFirstIndexBreakingNeckline(valueArr, necklineValue, operator):
 	if operator == "<":
