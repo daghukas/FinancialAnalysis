8afc0106f1f0d369c0ae0ef62bd5f12aeb94dd8a
daghukas <david.ghukasyan@student.hs-rm.de>
2022-06-03 17:27:58 +0200

    added calculation of stop loss

--- calcformations.py
+++ calcformations.py
@@ -84,14 +84,12 @@ def detect_double_formation(type_of_double_formation, chart_data):
         neckline_operator = "<"
         arr_index_extreme_values = sp.argrelextrema(np.array(dataset_close_val), np.greater)[0]
         arr_vals_extreme_values = dataset_close_val.iloc[arr_index_extreme_values]['Close'].values
-        title = "Top"
 
     elif type_of_double_formation == "1":
         crit_compare_extreme_vals = '<='
         neckline_operator = ">"
         arr_index_extreme_values = sp.argrelextrema(np.array(dataset_close_val), np.less)[0]
         arr_vals_extreme_values = dataset_close_val.iloc[arr_index_extreme_values]['Close'].values
-        title = "Bottom"
 
     #polyfit = np.polyfit(chart_data.index.tolist(), dataset_close_val, 15)
     #yvalpoly = np.polyval(polyfit, chart_data.index.tolist())
@@ -129,12 +127,14 @@ def detect_double_formation(type_of_double_formation, chart_data):
                     found_formations_index.append(index_dataset)
 
                     price_target = calc_price_target(curr_extreme, neckline_operator)
+                    stop_loss = calc_stop_loss(neckline_operator)
                     print("Take profits at Price Target of",price_target)
                     found_price_targets.append([price_target, min(arr_range_extremes),
                     max(arr_range_extremes)])
-                    print("Detected Double", title, ":")
-                    print(arr_values_of_extremes.index.values)
-                    print(arr_values_of_extremes['Close'].values)
+                    print("Set stop loss at:", stop_loss)
+                    print("Indizes:", arr_values_of_extremes.index.values)
+                    print("Values:", arr_values_of_extremes['Close'].values)
+                    print('\n')
 
     # zeichnen --> eigene Methode
     plot_formations(found_formations, found_formations_index, dataset_close_val)
@@ -146,6 +146,14 @@ def calc_price_target(snd_extrempoint, operator):
         return neckline_value - (snd_extrempoint - neckline_value)
 
     return neckline_value + (neckline_value - snd_extrempoint)
+
+def calc_stop_loss(operator):
+    """Calculate where to set the stop loss."""
+
+    if operator == "<":
+        return neckline_value * 1.01
+
+    return neckline_value * 0.99
 
 def get_first_index_breaking_neckline(range_arr, value_arr, operator):
     """Gets the first index, which breaks the neckline."""
@@ -168,11 +176,13 @@ def get_first_index_breaking_neckline(range_arr, value_arr, operator):
                     #print(range_arr)
                     #print(value_arr)
                     found_necklines.append([neckline_value, start_neckline, end_neckline])
+                    print("DETECTED DOUBLE TOP")
                     print("Short setzen und")
                     return index+1
             elif operator == ">":
                 if val > neckline_value:
                     found_necklines.append([neckline_value, start_neckline, end_neckline])
+                    print("DETECTED DOUBLE BOTTOM")
                     print("Long gehen und")
                     return index+1
 
